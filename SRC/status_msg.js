016fb1053249ae6ce8574c76ad044cf34d012d50
00-dh <kimdh081599@gmail.com>
2022-09-10 00:26:04 +0900

    🎆Style: submit message box hidden when user submit form

--- SRC/status_msg.js
+++ SRC/status_msg.js
@@ -2,11 +2,16 @@ const statusText = document.querySelector("textarea");
 const statusForm = document.querySelector(
   ".left_box__profile__status-message_box"
 );
+const resultText = document.querySelector(".textarea");
 
 function uploadStatusMsg(event) {
   event.preventDefault();
-  console.log(statusText.value);
+  const result = statusText.value;
+  resultText.innerText = result;
   statusText.value = "";
+  statusText.classList.add("hidden");
 }
 
-statusForm.addEventListener("click", uploadStatusMsg);
+/* needed bug fix first, resultText div space */
+/* needed bug fix second, resultText font color & text align */
+statusForm.addEventListener("submit", uploadStatusMsg);
