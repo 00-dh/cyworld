a3a10abf4b7e686ad5427b6aaaed643dcfc9beb5
00-dh <kimdh081599@gmail.com>
2022-09-11 19:02:48 +0900

    🛠Fix: btn keep seeing situation when submit the form

--- SRC/status_msg.js
+++ SRC/status_msg.js
@@ -2,6 +2,7 @@ const statusText = document.querySelector("textarea");
 const statusForm = document.querySelector(
   ".left_box__profile__status-message_box"
 );
+const statusBtn = document.querySelector("button");
 const resultText = document.querySelector(".textarea");
 
 function uploadStatusMsg(event) {
@@ -10,6 +11,8 @@ function uploadStatusMsg(event) {
   resultText.innerText = result;
   statusText.value = "";
   statusText.classList.add("hidden");
+  statusBtn.classList.add("hidden");
+  resultText.classList.remove("hidden");
 }
 
 /* needed bug fix first, resultText div space */
